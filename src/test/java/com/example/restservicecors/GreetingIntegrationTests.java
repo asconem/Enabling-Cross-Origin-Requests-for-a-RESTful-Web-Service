746c28e5fa85e281bd07e5ccb1dcc483dd776939
Matthew Ascone <asconem@gmail.com>
2020-07-12 19:12:00 -0400

    Add corsWithJavaconfig test

--- src/test/java/com/example/restservicecors/GreetingIntegrationTests.java
+++ src/test/java/com/example/restservicecors/GreetingIntegrationTests.java
@@ -30,3 +30,20 @@ public class GreetingIntegrationTests {
         Greeting greeting = entity.getBody();
         assertEquals("Hello, World!", greeting.getContent());
     }
+
+    @Test
+    public void corsWithJavaconfig() {
+        ResponseEntity<Greeting> entity = this.restTemplate.exchange(
+                RequestEntity.get(uri("/greeting-javaconfig")).header(HttpHeaders.ORIGIN, "http://localhost:9000").build(),
+                Greeting.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertEquals("http://localhost:9000", entity.getHeaders().getAccessControlAllowOrigin());
+        Greeting greeting = entity.getBody();
+        assertEquals("Hello, World!", greeting.getContent());
+    }
+
+    private URI uri(String path) {
+        return restTemplate.getRestTemplate().getUriTemplateHandler().expand(path);
+    }
+
+}
